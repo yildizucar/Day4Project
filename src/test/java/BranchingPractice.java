aac2edcc5f62eb2ea3eae2eb622eb48782c10386
yildiz ucar <yildizucr@gmail.com>
2021-05-25 19:23:42 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,6 +2,12 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
+        //cretae new project
+        //enabled versioan control
+        //added .gitignore with .idea.target foldr and iml file
+        //made initial commit by keeping track of all files
+        //shared  project on git hub
+
     }
 
 
